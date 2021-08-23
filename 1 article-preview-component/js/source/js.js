f3f794c74bf3b369242d1a6e9a92d7bd2b0e931b
maicol31 <86789808+maicol31@users.noreply.github.com>
2021-08-23 16:45:50 -0500

    con funciones

--- 1 article-preview-component/js/source/js.js
+++ 1 article-preview-component/js/source/js.js
@@ -1,55 +1,54 @@
+
 let contenedor = document.createElement("div");
+function crearmain(){
 contenedor.setAttribute("class" , "contenedor");
 document.body.append(contenedor);
+}
+crearmain();
 
-let imagen = document.createElement("div");
-imagen.setAttribute("class" , "imagen");
+function crearE(elemento, att, valor ){
+     let crear = document.createElement(elemento);
+     crear.setAttribute(att, valor);
+     return crear;
+}
+
+let imagen =  crearE("div", "class","imagen");
 contenedor.append(imagen);
 
-let contenedort = document.createElement("div");
-contenedort.setAttribute("class" , "contenedort");
+let contenedort =crearE( "div","class" , "contenedort");
 contenedor.append(contenedort);
 
-let texto = document.createElement("p"); 
-texto.setAttribute("class" , "texto");
+let texto = crearE("p", "class" , "texto");
 texto.innerText="Shift the overall look and feel by adding thesewonderful touches to furniture in your home";
 contenedort.append(texto);
 
-let texto2 = document.createElement("h2");
-texto2.setAttribute("class", "texto2");
+let texto2 = crearE("h2", "class", "texto2");
 texto2.innerText="Ever been in room and feit like something was missing? perhaps it felt slightly bare and uninviting I’ve got some simple tips to help you make any room feel complete";
 contenedort.append(texto2);
 
-let barrainf = document.createElement("div");
-barrainf.setAttribute("class" , "barrainf");
+let barrainf = crearE("div", "class" , "barrainf");
 contenedort.append(barrainf);
 
-let img = document.createElement("img");
-img.setAttribute("class" , "logo");
+let img = crearE("img", "class" , "logo");
 img.src = "./images/avatar-michelle.jpg"
 barrainf.append(img);
 
-let connombre = document.createElement("div");
-connombre.setAttribute("class" , "connombre");
+let connombre = crearE("div", "class" , "connombre");
 barrainf.append(connombre);
 
-let michelle = document.createElement("h3");
-michelle.setAttribute("class" , "michelle");
+let michelle = crearE("div", "class" , "michelle");
 michelle.innerText="Michelle  Appleton";
 connombre.append(michelle);
 
-let fecha = document.createElement("h4");
-fecha.setAttribute("class" , "fecha");
+let fecha = crearE("div", "class" , "fecha");
 fecha.innerText="28 Jun 2020";
 connombre.append(fecha);
 
-let compartir = document.createElement("div");
-compartir.setAttribute("class" , "compartir")
+let compartir = crearE("div", "class" , "compartir")
 barrainf.append(compartir);
 
-let compartir2 = document.createElement("img");
+let compartir2 = crearE("img", "id" , "botonc")
 compartir2.src = "./images/Group 2.svg";
-compartir2.setAttribute("id" , "botonc")
 barrainf.append(compartir2);
 
 
@@ -57,11 +56,9 @@ let redes = document.createElement("img");
 redes.src = "./images/barra.svg";
 compartir.append(redes);
 
-let redes2 = document.createElement("img");
+let redes2 = crearE("img","class" , "redes2")
 redes2.src = "./images/movil.svg";
-redes2.setAttribute("class" , "redes2");
 compartir.append(redes2);
-
 
 function hover(){
     compartir2.src = "./images/Group 1.svg";
@@ -72,6 +69,5 @@ function hover2(){
     compartir2.src = "./images/Group 2.svg";
     redes.style.display = "none"; 
 }
-
 compartir2.addEventListener("mouseenter", ()=>{hover()});
 compartir2.addEventListener("mouseleave", ()=>{hover2()});
